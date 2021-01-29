032d9a882c432e8ffe9bd45779df1cf116560582
Drew Bradley <williamandrewb@gmail.com>
2021-01-29 09:03:50 -0700

    Hook up index.js to HTML.

---
+++ src/components/GifGetter.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+class GifGetter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      SWCharacter: [],
+      randomCharacter: "" 
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.dev/api/people/")
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
+  handClick() {
+    const randomNum = Math.floor(Math.random() * this.state.SWCharacter.length)
+    console.log("This Got Clicked", randomNum)
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>{this.state.randomCharacter.name}</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1024px-Star_Wars_Logo.svg.png"/>
+        <button onClick={this.handClick}>Get New Character</button>
+      </main>
+    )
+  }
+}
+
+export default GifGetter
